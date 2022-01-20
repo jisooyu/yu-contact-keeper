08ac44fd18d64a8723829a0824caafb52232440f
jisooyu <jisooyu@hotmail.com>
2022-01-20 20:58:06 +0900

    up to 62 including alert

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/about' element={<About />} />
-            </Routes>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Routes>
+                  <Route exact path='/' element={<Home />} />
+                  <Route exact path='/about' element={<About />} />
+                  <Route exact path='/register' element={<Register />} />
+                  <Route exact path='/login' element={<Login />} />
+                </Routes>
+              </div>
+            </>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
